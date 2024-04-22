9aa007af64169e256cf5ebd0e741514410b9f16c
rahulkumarbudhwani1 <32469024+rahulkumarbudhwani1@users.noreply.github.com>
2024-04-23 00:37:11 +0200

    Update empty_uart_sensor.cpp

--- custom_components/empty_uart_sensor/empty_uart_sensor.cpp
+++ custom_components/empty_uart_sensor/empty_uart_sensor.cpp
@@ -7,7 +7,20 @@ namespace empty_uart_sensor {
 static const char *TAG = "empty_uart_sensor.sensor";
 
 void EmptyUARTSensor::setup() {
+    // Set up UART communication with specific serial configurations
+    this->begin(9600, SERIAL_8E1);
+    byte enableConfigCommand[] = {0x44, 0x11, 0x00, 0x00, 0x55, 0x4B};
+    this->write_array(enableConfigCommand, sizeof(enableConfigCommand));
+    delay(1000);
 
+    if (available() > 1) {
+      uint8_t received_data;
+      read_array(received_data, 1);
+      handle_data(received_data);
+      ESP_LOGD("LD6001", "Received data: %s", format_hex(received_data).c_str());
+    }
+
+    
 }
 
 void EmptyUARTSensor::update() {
